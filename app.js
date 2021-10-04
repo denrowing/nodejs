7be87830da77f27dd36838d0687b675ee6eeb1a9
Denys Burdin <denrowing@gmail.com>
2021-10-04 17:34:47 +0300

    init commit

--- app.js
+++ app.js
@@ -1,72 +1,7 @@
-const fs = require('fs')
-const path = require('path')
-
-const appendFilePath = path.join(__dirname, 'files', 'write.txt')
-const  mkdirPath = path.join(__dirname, 'files', 'user', 'photos')
-const newFilePath = (mkdirPath, 'newFile.txt')
-
-// const builder = require('./helper/user.builder')
-//
-// let user = builder.createUser('Denys', 33)
-//
-// console.log(user);
-
-// console.log(__dirname);
-//
-// fs.readFile(`${__dirname}/files/test.txt`, ((err, data) => {  // read file
-//     if (err) {
-//         console.log(err)
-//         return;
-//     }
-//     console.log(data.toString());
-// }))
-//
-//
-// fs.writeFile(`${__dirname}/files/write.txt`, 'Hello', (err) => {
-//     console.log(err);
-// })
-//
-// fs.appendFile(appendFilePath, 'Hello World 33\n', (err) => {  // add something to file
-//     console.log(err);
-// })
-//
-// fs.mkdir(mkdirPath, {recursive: true},(e) => {  // add directory/folders
-//     console.log(e);
-// })
-//
-// fs.unlink(appendFilePath, err => { //delete file
-//     console.log(err);
-// })
-
-fs.rename(
-    appendFilePath,
-    newFilePath,  // replace and rename file
-    err => {
-        console.log(err);
-    }
-    )
-
-fs.stat(newFilePath, (err, stats) => {
-    if (err) {
-        return
-    }
-    console.log(stats);
-})
-
-fs.readdir(newFilePath, (err, data) => {
-    if(err) {
-        console.log(err);
-        return
-    }
-    console.log(data);
-})
-
-// fs.rmdir()
-
-// Створити 2 папки хлопчики та дівчинкию
-// В них створити по 3 файли з обектами {name: 'Svs', gender: 'female'}
-// Create readdir
-
-
-
-
+// / - у вас є масив юзрів (до 10), з такими полями наприклад - const users = [
+// //     { name: 'olya', gender: 'female', age: 20 }
+// //         ...
+// // ], вам потрібно написати метод який створює файлики - де назва файлику - це імя вашого юзера (наприклад - Olya.txt),
+// вміст це сам ваш юзер - { name: 'olya', gender: 'female', age: 20 }
+// перед тим створити 4 папки (програмно) - наприклад - manOlder20, manYounger20, womanOlder20, womanYounger20
+// і розподілити ваших юзерів саме по відповідних папках
