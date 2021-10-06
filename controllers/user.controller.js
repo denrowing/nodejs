100fbb7eb4898e9c07ae27265d3c8bd4fdc80173
Denys Burdin <denrowing@gmail.com>
2021-10-06 15:29:48 +0300

    init commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,12 +5,17 @@ module.exports = {
         res.json(db)
     },
 
-    getUsersbyId: (req, res) => {
-        res.json(db)
+    getUserById: (req, res) => {
+       const { user_id } = req.params
+
+        const user = db[user_id - 1]
+        res.json({ user })
     },
 
     createUser: (req, res) => {
-        res.json('CREATE USER')
+        console.log(req.body);
+        db.push({...req.body, id: db.length + 1})
+        res.json(db)
     },
 
     updateUser: (req, res) => {
