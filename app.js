61c88ccf54dd145dc89be4d27f9f81a350bdb3b8
Denys Burdin <denrowing@gmail.com>
2021-10-07 10:49:18 +0300

    init commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded( {extended: true} ))
+
+const userRouter = require('./routes/user.router')
+
+app.use('/users', userRouter)
 
 app.listen(5000, () => {
     console.log(`App listen 5000`)
